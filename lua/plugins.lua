13f2cdeda5bdcddf2d1f341590e8e67e823330ed
lewpoly <ldixon.design@gmail.com>
2021-12-18 17:29:08 -0500

    made directories easier to navigate, condensed init.lua.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,97 +1,111 @@
-
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  return
+	return
 end
 
 -- Have packer use a popup window
-packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-  },
-}
-
-require('packer').startup(function(use)
-
-  use 'wbthomason/packer.nvim'
-
-  -- LSP
-  use 'neovim/nvim-lspconfig'
-  use 'onsails/lspkind-nvim'
-  use 'folke/lsp-colors.nvim'
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use { 'folke/trouble.nvim', requires = "kyazdani42/nvim-web-devicons" }
-
-  -- Telescope
-  use 'nvim-telescope/telescope.nvim'
-  use 'nvim-telescope/telescope-media-files.nvim'
-  use 'nvim-lua/popup.nvim'
-  use 'nvim-lua/plenary.nvim'
-  use 'BurntSushi/ripgrep'
-
-  -- Completion
-  use 'rafamadriz/friendly-snippets'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-nvim-lua'
-  use {'tzachar/cmp-tabnine', run='./install.sh', requires = 'hrsh7th/nvim-cmp'}
-  use 'L3MON4D3/LuaSnip'
-  use 'saadparwaiz1/cmp_luasnip'
-  use 'kdheepak/cmp-latex-symbols'
-  use 'hrsh7th/cmp-path'
-  use 'windwp/nvim-autopairs'
-  use "blackCauldron7/surround.nvim"
-  use 'windwp/nvim-ts-autotag'
-
-  -- Color
-  use 'norcalli/nvim-colorizer.lua'
-  -- use 'christianchiarulli/nvcode-color-schemes.vim'
-  use 'rmehri01/onenord.nvim'
-  use 'EdenEast/nightfox.nvim'
-  use 'LunarVim/Colorschemes'
-  use 'Shatur/neovim-ayu'
-
-  -- Formatting
-  use({
-    "jose-elias-alvarez/null-ls.nvim",
-    config = function()
-        require("null-ls").setup()
-    end,
-    requires = { "nvim-lua/plenary.nvim" },
+packer.init({
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
 })
 
-  -- Tree
-  use {
-    'kyazdani42/nvim-tree.lua',
-    requires = 'kyazdani42/nvim-web-devicons',
-    config = function() require'nvim-tree'.setup {} end
-}
-  -- Git
-  use {
-    'lewis6991/gitsigns.nvim',
-    requires = {
-      'nvim-lua/plenary.nvim'
-    },
-     tag = 'release' -- To use the latest release
-  }
+require("packer").startup(function(use)
+	use("wbthomason/packer.nvim")
 
-  -- ToggleTerm
-  use {"akinsho/toggleterm.nvim"}
+	-- LSP
+	use("neovim/nvim-lspconfig")
+	use("onsails/lspkind-nvim")
+	use("folke/lsp-colors.nvim")
+	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+	use({ "folke/trouble.nvim", requires = "kyazdani42/nvim-web-devicons" })
 
-  -- Lualine
-  use 'nvim-lualine/lualine.nvim'
+	-- Telescope
+	use("nvim-telescope/telescope.nvim")
+	use("nvim-telescope/telescope-media-files.nvim")
+	use("nvim-lua/popup.nvim")
+	use("nvim-lua/plenary.nvim")
+	use("BurntSushi/ripgrep")
 
-  -- Bufferline
-  use 'akinsho/bufferline.nvim'
+	-- Completion
+	use("rafamadriz/friendly-snippets")
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-nvim-lua")
+	use({ "tzachar/cmp-tabnine", run = "./install.sh", requires = "hrsh7th/nvim-cmp" })
+	use("L3MON4D3/LuaSnip")
+	use("saadparwaiz1/cmp_luasnip")
+	use("kdheepak/cmp-latex-symbols")
+	use("hrsh7th/cmp-path")
+	use("windwp/nvim-autopairs")
+	use("windwp/nvim-ts-autotag")
+	use({
+		"blackCauldron7/surround.nvim",
+		config = function()
+			require("surround").setup({ mappings_style = "sandwich" })
+		end,
+	})
 
-  -- Dashboard
-  use 'glepnir/dashboard-nvim'
+	-- Color
+	use({
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup()
+		end,
+	})
+	-- use 'christianchiarulli/nvcode-color-schemes.vim'
+	use("rmehri01/onenord.nvim")
+	use("EdenEast/nightfox.nvim")
+	use("LunarVim/Colorschemes")
+	use("Shatur/neovim-ayu")
 
-  -- Comment
-  use 'terrortylor/nvim-comment'
+	-- Formatting
+	use({
+		"jose-elias-alvarez/null-ls.nvim",
+		config = function()
+			require("null-ls").setup()
+		end,
+		requires = { "nvim-lua/plenary.nvim" },
+	})
 
+	-- Tree
+	use({
+		"kyazdani42/nvim-tree.lua",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = function()
+			require("nvim-tree").setup({})
+		end,
+	})
+	-- Git
+	use({
+		"lewis6991/gitsigns.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+		},
+		tag = "release", -- To use the latest release
+	})
+
+	-- ToggleTerm
+	use({ "akinsho/toggleterm.nvim" })
+
+	-- Lualine
+	use("nvim-lualine/lualine.nvim")
+
+	-- Bufferline
+	use("akinsho/bufferline.nvim")
+
+	-- Dashboard
+	use("glepnir/dashboard-nvim")
+
+	-- Comment
+	use({
+		"terrortylor/nvim-comment",
+		config = function()
+			require("nvim_comment").setup()
+		end,
+	})
 end)
--- lua/user/autopairs.lua
+++ lua/user/autopairs.lua
@@ -16,6 +16,27 @@ cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex 
 local npairs = require'nvim-autopairs'
 local Rule   = require'nvim-autopairs.rule'
 
+npairs.setup {
+  check_ts = true,
+  ts_config = {
+    lua = { "string", "source" },
+    javascript = { "string", "template_string" },
+    java = false,
+  },
+  disable_filetype = { "TelescopePrompt", "spectre_panel" },
+  fast_wrap = {
+    map = "<M-e>",
+    chars = { "{", "[", "(", '"', "'" },
+    pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+    offset = 0, -- Offset from pattern match
+    end_key = "$",
+    keys = "qwertyuiopzxcvbnmasdfghjkl",
+    check_comma = true,
+    highlight = "PmenuSel",
+    highlight_grey = "LineNr",
+  },
+}
+
 npairs.add_rules {
   Rule(' ', ' ')
     :with_pair(function (opts)
