4fce52b1e7682db35bb48486ece35373a1eec90c
lewpoly <ldixon.design@gmail.com>
2022-01-20 22:40:04 -0500

    working on getting css colors with colorizer

--- lua/core/lsp/null-ls.lua
+++ lua/core/lsp/null-ls.lua
@@ -7,18 +7,18 @@ end
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 -- local diagnostics = null_ls.builtins.diagnostics
-local code_actions = null_ls.builtins.code_actions
+-- local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
 	debug = false,
 	sources = {
 		formatting.eslint_d,
-		formatting.prettier.with({
-			extra_args = { disable_filetypes = { "html" }, "--no-semi", "--single-quote", "--jsx-single-quote" },
-		}),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
 		formatting.prettierd,
+		-- formatting.prettier.with({
+		-- 	extra_args = { disable_filetypes = { "html" }, "--no-semi", "--single-quote", "--jsx-single-quote" },
+		-- }),
 		-- diagnostics.flake8
 	},
 })
