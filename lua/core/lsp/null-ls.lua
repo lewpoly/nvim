fdbb4df10c238eb37d4580230ddb479b4bd26f6f
lewpoly <ldixon.design@gmail.com>
2022-01-23 00:58:53 -0500

    html formatting fix

--- lua/core/lsp/null-ls.lua
+++ lua/core/lsp/null-ls.lua
@@ -12,10 +12,11 @@ null_ls.setup({
 	debug = false,
 	sources = {
 		formatting.prettier.with({
-      disabled_filetypes = { "html", "css" },
-      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-    formatting.eslint_d,
-    formatting.prettierd,
+			disabled_filetypes = { "html", "css" },
+			extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+		}),
+		formatting.eslint_d,
+		formatting.prettierd.with({ disabled_filetypes = { "html" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
 		-- diagnostics.flake8
