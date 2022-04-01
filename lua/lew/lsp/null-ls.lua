3eaf3d6d72ab68befcc07e737acfa0ac352ce16c
lewpoly <ldixon.design@gmail.com>
2022-03-31 21:55:00 -0400

    just gonna have to make rc's. prettierd is so much faster.

--- lua/lew/lsp/null-ls.lua
+++ lua/lew/lsp/null-ls.lua
@@ -11,20 +11,13 @@ local formatting = null_ls.builtins.formatting
 null_ls.setup({
 	debug = true,
 	sources = {
-		-- formatting.prettierd.with({
-		-- 	formatStdin = true,
-		-- 	env = {
-		-- 		PRETTIERD_DEFAULT_CONFIG = "/home/lew/.config/nvim/.prettierrc.json",
-		-- 	},
-		-- }),
 		formatting.prettierd,
-		formatting.prettier.with({
-			disabled_filetypes = { "html", "css" },
-			extra_args = { "--single-quote", "--jsx-single-quote" },
-		}),
+		-- formatting.prettier.with({
+		-- 	disabled_filetypes = { "html", "css" },
+		-- 	extra_args = { "--single-quote", "--jsx-single-quote" },
+		-- }),
 		-- formatting.eslint_d,
 		formatting.stylua,
-		-- diagnostics.flake8
 		formatting.black.with({ extra_args = { "--fast" } }),
 	},
 })
