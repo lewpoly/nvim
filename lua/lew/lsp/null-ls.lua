5d5e03a720f730ddd6586508b4b2c10bb7c0564f
Lew Dixon <ldixon.design@gmail.com>
2022-04-22 13:24:12 -0400

    added eslint_d for diagnostics and code actions

--- lua/lew/lsp/null-ls.lua
+++ lua/lew/lsp/null-ls.lua
@@ -7,16 +7,24 @@ end
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 local formatting = null_ls.builtins.formatting
+local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup {
   debug = true,
   sources = {
     formatting.prettierd,
+    diagnostics.tidy,
     -- formatting.prettier.with({
     -- 	disabled_filetypes = { "html", "css" },
     -- 	extra_args = { "--single-quote", "--jsx-single-quote" },
     -- }),
-    -- formatting.eslint_d,
+    code_actions.eslint_d.with({
+      prefer_local = "node_modules/.bin"
+    }),
+    diagnostics.eslint_d.with({
+      prefer_local = "node_modules/.bin"
+    }),
+    -- diagnostics.eslint_d,
     formatting.stylua,
     formatting.black.with { extra_args = { "--fast" } },
   },
