ddc67788ea8c65b4fe88696ec192a214dd574653
lewpoly <ldixon.design@gmail.com>
2022-04-30 16:36:40 -0400

    changed options, but removed plugin. causing grep problems

--- lua/lew/colorizer.lua
+++ lua/lew/colorizer.lua
@@ -8,7 +8,10 @@ colorizer.setup({ "*" }, {
   RRGGBB = true, -- #RRGGBB hex codes
   RRGGBBAA = true, -- #RRGGBBAA hex codes
   rgb_fn = true, -- CSS rgb() and rgba() functions
-  hsl_fn = true, -- CSS hsl() and hsla() functions
-  css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+  hsl_fn = false, -- CSS hsl() and hsla() functions
+  css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
   css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+  names = false, -- "Name codes ie blue or Red"
+  -- Available modes: foreground, background, virtualtext
+  mode = "background", -- Set the display mode.)
 })
