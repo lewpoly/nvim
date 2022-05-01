b8b28297a1acd4a5ad0467b9c207e321b98ca1f4
lewpoly <ldixon.design@gmail.com>
2022-04-30 23:19:50 -0400

    renamer offset encoding fix

--- lua/lew/renamer.lua
+++ lua/lew/renamer.lua
@@ -44,3 +44,10 @@ renamer.setup {
     ["<c-r>"] = mappings_utils.redo,
   },
 }
+renamer._apply_workspace_edit = function(resp)
+  local params = vim.lsp.util.make_position_params()
+  local results_lsp, _ = vim.lsp.buf_request_sync(0, require("renamer.constants").strings.lsp_req_rename, params)
+  local client_id = results_lsp and next(results_lsp) or nil
+  local client = vim.lsp.get_client_by_id(client_id)
+  require("vim.lsp.util").apply_workspace_edit(resp, client.offset_encoding)
+end
