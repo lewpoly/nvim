def7b9dcad0b758f357b0817cbe4fe789c7eb8a7
lewpoly <ldixon.design@gmail.com>
2022-05-11 18:19:11 -0400

    removed typescript.nvim

--- lua/lew/lsp/typescript.lua
+++
@@ -1,8 +0,0 @@
-require("typescript").setup({
-    disable_commands = false, -- prevent the plugin from creating Vim commands
-    disable_formatting = false, -- disable tsserver's formatting capabilities
-    debug = true, -- enable debug logging for commands
-    server = { -- pass options to lspconfig's setup method
-        -- on_attach = ...,
-    },
-})
