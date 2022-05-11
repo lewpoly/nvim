4a85d4ae51548cca9553bfd4690c24b6e586bc16
Lew Dixon <ldixon.design@gmail.com>
2022-05-11 14:05:04 -0400

    changed to new lsp-installer setup method

--- lua/lew/lsp/lsp-installer.lua
+++ lua/lew/lsp/lsp-installer.lua
@@ -3,21 +3,84 @@ if not status_ok then
   return
 end
 
-lsp_installer.on_server_ready(function(server)
-  local opts = {
+local servers = {
+  "cssls",
+  "cssmodules_ls",
+  "emmet_ls",
+  "html",
+  "clangd",
+  "jsonls",
+  "yamlls",
+  "sumneko_lua",
+  "tsserver",
+  "pyright",
+}
+
+local settings = {
+  ensure_installed = servers,
+  -- automatic_installation = false,
+  ui = {
+    icons = {
+      -- server_installed = "◍",
+      -- server_pending = "◍",
+      -- server_uninstalled = "◍",
+      -- server_installed = "✓",
+      -- server_pending = "➜",
+      -- server_uninstalled = "✗",
+    },
+    keymaps = {
+      toggle_server_expand = "<CR>",
+      install_server = "i",
+      update_server = "u",
+      check_server_version = "c",
+      update_all_servers = "U",
+      check_outdated_servers = "C",
+      uninstall_server = "X",
+    },
+  },
+
+  log_level = vim.log.levels.INFO,
+  -- max_concurrent_installers = 4,
+  -- install_root_dir = path.concat { vim.fn.stdpath "data", "lsp_servers" },
+}
+
+lsp_installer.setup(settings)
+
+local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_status_ok then
+  return
+end
+
+local opts = {}
+
+for _, server in pairs(servers) do
+  opts = {
     on_attach = require("lew.lsp.handlers").on_attach,
     capabilities = require("lew.lsp.handlers").capabilities,
   }
 
-  if server.name == "jsonls" then
+  if server == "jsonls" then
     local jsonls_opts = require "lew.lsp.settings.jsonls"
     opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
   end
 
-  if server.name == "sumneko_lua" then
+  if server == "sumneko_lua" then
     local sumneko_opts = require "lew.lsp.settings.sumneko_lua"
     opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
   end
 
-  server:setup(opts)
-end)
+  if server == "pyright" then
+    local pyright_opts = require "lew.lsp.settings.pyright"
+    opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+  end
+
+  -- if server == "emmet_ls" then
+  --   local emmet_ls_opts = require "lew.lsp.settings.emmet_ls"
+  --   opts = vim.tbl_deep_extend("force", emmet_ls_opts, opts)
+  -- end
+
+  lspconfig[server].setup(opts)
+end
+
+-- TODO: add something to installer later
+-- require("lspconfig").motoko.setup {}
