e3f6523db0586ba9c2f2d92ce61d4e7cb66c2921
lewpoly <ldixon.design@gmail.com>
2022-05-20 23:27:02 -0400

    added cybu and auto cmd

--- lua/lew/autocommands.lua
+++ lua/lew/autocommands.lua
@@ -41,3 +41,46 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
     vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
   end,
 })
+
+-- Winbar
+local function has_value(tab, val)
+  for _, value in ipairs(tab) do
+    if value == val then
+      return true
+    end
+  end
+
+  return false
+end
+
+vim.api.nvim_create_autocmd({ "CursorMoved", "BufWinEnter", "BufFilePost" }, {
+  callback = function()
+    local winbar_filetype_exclude = {
+      "help",
+      "startify",
+      "dashboard",
+      "packer",
+      "neogitstatus",
+      "NvimTree",
+      "Trouble",
+      "alpha",
+      "lir",
+      "Outline",
+      "spectre_panel",
+      "toggleterm",
+    }
+
+    if vim.tbl_contains(winbar_filetype_exclude, vim.bo.filetype) then
+      vim.opt_local.winbar = nil
+      return
+    end
+
+    local value = require("lew.winbar").gps()
+
+    if value == nil then
+      value = require("lew.winbar").filename()
+    end
+
+    vim.opt_local.winbar = value
+  end,
+})
---
+++ lua/lew/cybu.lua
@@ -0,0 +1,46 @@
+local status_ok, cybu = pcall(require, "cybu")
+if not status_ok then
+  return
+end
+
+cybu.setup {
+  position = {
+    relative_to = "win", -- win, editor, cursor
+    anchor = "bottomright", -- topleft, topcenter, topright,
+    -- centerleft, center, centerright,
+    -- bottomleft, bottomcenter, bottomright
+    vertical_offset = 0, -- vertical offset from anchor in lines
+    horizontal_offset = 0, -- vertical offset from anchor in columns
+    max_win_height = 5, -- height of cybu window in lines
+    max_win_width = 0.5, -- integer for absolute in columns
+    -- float for relative to win/editor width
+  },
+  style = {
+    path = "relative", -- absolute, relative, tail (filename only)
+    border = "rounded", -- single, double, rounded, none
+    separator = " ", -- string used as separator
+    prefix = "…", -- string used as prefix for truncated paths
+    padding = 1, -- left & right padding in number of spaces
+    devicons = {
+      enabled = true, -- enable or disable web dev icons
+      colored = true, -- enable color for web dev icons
+    },
+    highlights = { -- see highlights via :highlight
+      current_buffer = "Visual", -- used for the current buffer
+      adjacent_buffers = "Comment", -- used for buffers not in focus
+      background = "Normal", -- used for the window background
+    },
+  },
+  display_time = 1500, -- time the cybu window is displayed
+  exclude = { -- filetypes, cybu will not be active
+    "NvimTree",
+    "toggleterm",
+    "qf",
+    "help",
+    "packer",
+    "Trouble",
+    "Outline",
+    "alpha",
+  },
+  fallback = function() end,
+}
