a29ab91bf6989bb98f3a1f87b69e12aafe0d888e
lewpoly <ldixon.design@gmail.com>
2022-05-20 23:27:26 -0400

    useful function for tabline

---
+++ lua/lew/functions.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+function M.toggle_tabline()
+  local value = vim.api.nvim_get_option_value("showtabline", {})
+
+  if value == 2 then
+    value = 0
+  else
+    value = 2
+  end
+
+  vim.opt.showtabline = value
+
+  vim.notify("showtabline" .. " set to " .. tostring(value))
+end
+
+return M
