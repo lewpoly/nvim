73cc7c15a5ce3153c06b91d97b9432b99d9395a3
lewpoly <ldixon.design@gmail.com>
2022-05-28 19:39:15 -0400

    prevent formatting conflicts

--- lua/lew/lsp/handlers.lua
+++ lua/lew/lsp/handlers.lua
@@ -74,9 +74,9 @@ local function lsp_keymaps(bufnr)
 end
 
 M.on_attach = function(client, bufnr)
-  -- if client.name == "tsserver" or client.name == "html" or client.name == "jsonls" or client.name == "sumneko_lua" then
-  --   client.server_capabilities.document_formatting = false
-  -- end
+  if client.name == "tsserver" or client.name == "html" or client.name == "jsonls" or client.name == "sumneko_lua" then
+    client.server_capabilities.documentFormattingProvider = false
+  end
   lsp_keymaps(bufnr)
   lsp_highlight_document(client)
 end
