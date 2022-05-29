a0bd6cd471fe95822409dfe7636361ba815409d6
lewpoly <ldixon.design@gmail.com>
2022-05-29 14:01:09 -0400

    don't hide root folder

--- lua/lew/nvimtree.lua
+++ lua/lew/nvimtree.lua
@@ -117,7 +117,7 @@ nvim_tree.setup {
   view = {
     width = 30,
     height = 30,
-    hide_root_folder = true,
+    hide_root_folder = false,
     side = "left",
     mappings = {
       custom_only = false,
