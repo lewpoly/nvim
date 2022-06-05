2419b044256a8d363e8359d971bf0023f9c8e231
lewpoly <ldixon.design@gmail.com>
2022-06-05 17:02:40 -0400

    use_lsp_diagnostic deprecated, use use_diagnostic instead

--- lua/lew/trouble.lua
+++ lua/lew/trouble.lua
@@ -45,5 +45,5 @@ trouble.setup {
     information = "",
     other = "﫠",
   },
-  use_lsp_diagnostic_signs = true, -- enabling this will use the signs defined in your lsp client
+  use_diagnostic_signs = true, -- enabling this will use the signs defined in your lsp client
 }
