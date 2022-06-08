a00402490838b209a132ad3cf2abfd71cf7c62c0
Lew Dixon <ldixon.design@gmail.com>
2022-06-08 16:42:04 -0400

    leave winbar alone!!

--- lua/lew/winbar.lua
+++ lua/lew/winbar.lua
@@ -86,7 +86,7 @@ M.get_winbar = function()
   end
 
   if not f.isempty(value) and f.get_buf_option "mod" then
-    local mod = "%#Number#" .. require("lew.icons").ui.Circle .. "%*"
+    local mod = "%#CursorLineNr#" .. require("lew.icons").ui.Circle .. "%*"
     if gps_added then
       value = value .. " " .. mod
     else
