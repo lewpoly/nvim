57d9a0a2dc08fa2734c0c9bcb097c99bdaf69f43
lewpoly <ldixon.design@gmail.com>
2022-07-05 19:37:11 -0400

    luasnip autocmd

--- lua/lew/autocommands.lua
+++ lua/lew/autocommands.lua
@@ -58,3 +58,12 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     ]]
   end,
 })
+
+vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+  callback = function()
+    local luasnip = require "luasnip"
+    if luasnip.expand_or_jumpable() then
+      luasnip.unlink_current()
+    end
+  end,
+})
