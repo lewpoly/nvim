46a4cb91865ac5f821beb4125a69e827f799d482
lewpoly <ldixon.design@gmail.com>
2022-07-05 20:37:07 -0400

    removed comment

--- lua/lew/cmp.lua
+++ lua/lew/cmp.lua
@@ -1,9 +1,3 @@
--- nvim-cmp setup
--- local has_words_before = function()
---   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
---   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
--- end
-
 local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
   return
