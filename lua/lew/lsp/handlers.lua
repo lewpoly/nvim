b8666215bca062ecbf18fd667801a6b95865635c
lewpoly <ldixon.design@gmail.com>
2022-07-10 11:15:42 -0400

    bfs works

--- lua/lew/lsp/handlers.lua
+++ lua/lew/lsp/handlers.lua
@@ -50,12 +50,12 @@ M.setup = function()
 
   vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
     border = "rounded",
-    width = 60,
+    -- width = 60,
   })
 
   vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
     border = "rounded",
-    width = 60,
+    -- width = 60,
   })
 end
 
