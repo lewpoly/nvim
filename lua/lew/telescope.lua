4d7f7587f2122df84a2462976e8492ad8e352538
lewpoly <ldixon.design@gmail.com>
2022-07-17 22:29:04 -0400

    icons and ignore patterns

--- lua/lew/telescope.lua
+++ lua/lew/telescope.lua
@@ -5,15 +5,70 @@ end
 
 local actions = require "telescope.actions"
 telescope.load_extension "media_files"
--- local icons = require "lew.icons"
+local icons = require "lew.icons"
 
 telescope.setup {
   defaults = {
 
-    prompt_prefix = " ",
-    -- prompt_prefix = icons.ui.Telescope .. ": ",
-    selection_caret = " ",
+    -- prompt_prefix = " ",
+    prompt_prefix = icons.ui.Telescope .. " ",
+    -- selection_caret = " ",
+    selection_caret = icons.ui.ChevronRight .. " ",
     path_display = { "smart" },
+
+    file_ignore_patterns = {
+      ".git/",
+      "target/",
+      "docs/",
+      "vendor/*",
+      "%.lock",
+      "__pycache__/*",
+      "%.sqlite3",
+      "%.ipynb",
+      "node_modules/*",
+      -- "%.jpg",
+      -- "%.jpeg",
+      -- "%.png",
+      "%.svg",
+      "%.otf",
+      "%.ttf",
+      -- "%.webp",
+      ".dart_tool/",
+      ".github/",
+      ".gradle/",
+      ".idea/",
+      ".settings/",
+      ".vscode/",
+      "__pycache__/",
+      "build/",
+      "env/",
+      "gradle/",
+      "node_modules/",
+      "%.pdb",
+      "%.dll",
+      "%.class",
+      "%.exe",
+      "%.cache",
+      "%.ico",
+      "%.pdf",
+      "%.dylib",
+      "%.jar",
+      "%.docx",
+      "%.met",
+      "smalljre_*/*",
+      ".vale/",
+      "%.burp",
+      "%.mp4",
+      "%.mkv",
+      "%.rar",
+      "%.zip",
+      "%.7z",
+      "%.tar",
+      "%.bz2",
+      "%.epub",
+      "%.flac",
+      "%.tar.gz",
+    },
 
     mappings = {
       i = {
