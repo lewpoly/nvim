54079c2bcd124b2beb7345f241812316c7d53426
lewpoly <ldixon.design@gmail.com>
2022-07-24 15:39:27 -0400

    changed colors

--- lua/lew/alpha.lua
+++ lua/lew/alpha.lua
@@ -3,9 +3,17 @@ if not status_ok then
 	return
 end
 
+local dashboard = require "alpha.themes.dashboard"
+
+local function button(sc, txt, keybind, keybind_opts)
+  local b = dashboard.button(sc, txt, keybind, keybind_opts)
+  b.opts.hl_shortcut = "Macro"
+  return b
+end
+
 local icons = require("lew.icons")
 
-local dashboard = require("alpha.themes.dashboard")
+-- local dashboard = require("alpha.themes.dashboard")
 dashboard.section.header.val = {
 "                     ,                          ",
 "                ,.  | \\                        ",
@@ -38,19 +46,19 @@ dashboard.section.header.val = {
 	-- [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
 }
 dashboard.section.buttons.val = {
-	dashboard.button("f", icons.documents.Files .. " Find file", ":Telescope find_files <CR>"),
-	dashboard.button("e", icons.ui.NewFile .. " New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button(
+	button("f", icons.documents.Files .. " Find file", ":Telescope find_files <CR>"),
+	button("e", icons.ui.NewFile .. " New file", ":ene <BAR> startinsert <CR>"),
+	button(
 		"p",
 		icons.git.Repo .. " Find project",
 		":lua require('telescope').extensions.projects.projects()<CR>"
 	),
-	dashboard.button("r", icons.ui.History .. " Recent files", ":Telescope oldfiles <CR>"),
-	dashboard.button("t", icons.ui.List .. " Find text", ":Telescope live_grep <CR>"),
-	dashboard.button("s", icons.ui.SignIn .. " Find Session", ":Telescope sessions save_current=false <CR>"),
-	dashboard.button("c", icons.ui.Gear .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
-	dashboard.button("u", icons.ui.CloudDownload .. " Update", ":PackerSync<CR>"),
-	dashboard.button("q", icons.ui.SignOut .. " Quit", ":qa<CR>"),
+	button("r", icons.ui.History .. " Recent files", ":Telescope oldfiles <CR>"),
+	button("g", icons.ui.List .. " Find text", ":Telescope live_grep <CR>"),
+	button("s", icons.ui.SignIn .. " Find Session", ":Telescope sessions save_current=false <CR>"),
+	button("c", icons.ui.Gear .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
+	button("u", icons.ui.CloudDownload .. " Update", ":PackerSync<CR>"),
+	button("q", icons.ui.SignOut .. " Quit", ":qa<CR>"),
 }
 local function footer()
 	-- NOTE: requires the fortune-mod package to work
@@ -63,9 +71,9 @@ end
 
 dashboard.section.footer.val = footer()
 
-dashboard.section.footer.opts.hl = "String"
-dashboard.section.header.opts.hl = "String"
-dashboard.section.buttons.opts.hl = "Keyword"
+dashboard.section.footer.opts.hl = "Float"
+dashboard.section.header.opts.hl = "Float"
+dashboard.section.buttons.opts.hl = "Function"
 
 dashboard.opts.opts.noautocmd = true
 -- vim.cmd([[autocmd User AlphaReady echo 'ready']])
