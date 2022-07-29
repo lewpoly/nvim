391c2628ff2f058209bc626893bbf5ef204bed7a
lewpoly <ldixon.design@gmail.com>
2022-07-28 20:24:15 -0400

    cp is nutty

--- lua/lew/copilot.lua
+++ lua/lew/copilot.lua
@@ -1,3 +1,13 @@
+-- For copilot.vim
+-- vim.g.copilot_filetypes = {
+--   ["*"] = false,
+-- }
+
+-- vim.cmd [[
+--   imap <silent><script><expr> <C-A> copilot#Accept("\<CR>")
+--   let g:copilot_no_tab_map = v:true
+-- ]]
+
 local status_ok, copilot = pcall(require, "copilot")
 if not status_ok then
   return
@@ -6,12 +16,20 @@ end
 copilot.setup {
   cmp = {
     enabled = true,
-    method = "getPanelCompletions",
+    method = "getCompletionsCycling",
   },
   panel = { -- no config options yet
     enabled = true,
   },
   ft_disable = { "markdown" },
   -- plugin_manager_path = vim.fn.stdpath "data" .. "/site/pack/packer",
-  -- server_opts_overrides = {},
+  server_opts_overrides = {
+    -- trace = "verbose",
+    settings = {
+      advanced = {
+        -- listCount = 10, -- #completions for panel
+        inlineSuggestCount = 3, -- #completions for getCompletions
+      },
+    },
+  },
 }
