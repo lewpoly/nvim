a1b8357b45c05db3cb40f39c6b70469653024708
Lew Dixon <ldixon.design@gmail.com>
2022-09-15 12:00:59 -0400

    faster yank

--- lua/lew/autocommands.lua
+++ lua/lew/autocommands.lua
@@ -128,7 +128,7 @@ vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
 
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
   callback = function()
-    vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
+    vim.highlight.on_yank { higroup = "Visual", timeout = 50 }
   end,
 })
 
