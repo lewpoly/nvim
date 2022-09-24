53033c27da2bd90eecb943186693733955bee98c
lewpoly <ldixon.design@gmail.com>
2022-09-24 10:05:11 -0400

    fixed telescope binds

--- lua/lew/telescope.lua
+++ lua/lew/telescope.lua
@@ -3,14 +3,14 @@ if not status_ok then
   return
 end
 
-local h_status_ok, harpoon = pcall(require, "harpoon")
-if not h_status_ok then
-  return
-end
+-- local h_status_ok, harpoon = pcall(require, "harpoon")
+-- if not h_status_ok then
+--   return
+-- end
 
 local actions = require "telescope.actions"
 telescope.load_extension "media_files"
-telescope.load_extension "harpoon"
+-- telescope.load_extension "harpoon"
 local icons = require "lew.icons"
 
 telescope.setup {
@@ -22,78 +22,24 @@ telescope.setup {
     selection_caret = icons.ui.ChevronRight .. " ",
     path_display = { "smart" },
 
-    file_ignore_patterns = {
-      ".git/",
-      "target/",
-      "docs/",
-      "vendor/*",
-      "%.lock",
-      "__pycache__/*",
-      "%.sqlite3",
-      "%.ipynb",
-      "node_modules/*",
-      -- "%.jpg",
-      -- "%.jpeg",
-      -- "%.png",
-      "%.svg",
-      "%.otf",
-      "%.ttf",
-      -- "%.webp",
-      ".dart_tool/",
-      ".github/",
-      ".gradle/",
-      ".idea/",
-      ".settings/",
-      ".vscode/",
-      "__pycache__/",
-      "build/",
-      "env/",
-      "gradle/",
-      "node_modules/",
-      "%.pdb",
-      "%.dll",
-      "%.class",
-      "%.exe",
-      "%.cache",
-      "%.ico",
-      "%.pdf",
-      "%.dylib",
-      "%.jar",
-      "%.docx",
-      "%.met",
-      "smalljre_*/*",
-      ".vale/",
-      "%.burp",
-      "%.mp4",
-      "%.mkv",
-      "%.rar",
-      "%.zip",
-      "%.7z",
-      "%.tar",
-      "%.bz2",
-      "%.epub",
-      "%.flac",
-      "%.tar.gz",
-    },
-
     mappings = {
       i = {
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-        ["<S-Tab>"] = actions.move_selection_next,
-        ["<Tab>"] = actions.move_selection_previous,
+        ["<C-j>"] = require("telescope.actions").move_selection_next,
+        ["<C-k>"] = require("telescope.actions").move_selection_previous,
+        ["<S-Tab>"] = actions.move_selection_previous,
+        ["<Tab>"] = actions.move_selection_next,
 
         ["<C-c>"] = actions.close,
 
-        ["<Down>"] = actions.move_selection_next,
-        ["<Up>"] = actions.move_selection_previous,
+        -- ["<Down>"] = actions.move_selection_next,
+        -- ["<Up>"] = actions.move_selection_previous,
 
         ["<CR>"] = actions.select_default,
         ["<C-s>"] = actions.select_horizontal,
         ["<C-v>"] = actions.select_vertical,
         ["<C-t>"] = actions.select_tab,
 
-        ["<c-d>"] = require("telescope.actions").delete_buffer,
+        ["<c-d>"] = actions.delete_buffer,
 
         -- ["<C-u>"] = actions.preview_scrolling_up,
         -- ["<C-d>"] = actions.preview_scrolling_down,
@@ -117,8 +63,8 @@ telescope.setup {
         ["<C-t>"] = actions.select_tab,
         ["<c-d>"] = require("telescope.actions").delete_buffer,
 
-        ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+        ["<Tab>"] = actions.move_selection_next,
+        ["<S-Tab>"] = actions.move_selection_previous,
         ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
         ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
 
@@ -146,18 +92,19 @@ telescope.setup {
   pickers = {
     live_grep = {
       theme = "ivy",
+      previewer = true,
     },
     grep_string = {
       theme = "ivy",
     },
     find_files = {
       theme = "ivy",
-      previewer = true,
+      previewer = false,
     },
     buffers = {
       theme = "dropdown",
+      initial_mode = "insert",
       previewer = false,
-      initial_mode = "normal",
     },
     planets = {
       show_pluto = true,
@@ -184,10 +131,63 @@ telescope.setup {
       initial_mode = "normal",
     },
   },
+  file_ignore_patterns = {
+    ".git/",
+    "target/",
+    "docs/",
+    "vendor/*",
+    "%.lock",
+    "__pycache__/*",
+    "%.sqlite3",
+    "%.ipynb",
+    "node_modules/*",
+    -- "%.jpg",
+    -- "%.jpeg",
+    -- "%.png",
+    "%.svg",
+    "%.otf",
+    "%.ttf",
+    -- "%.webp",
+    ".dart_tool/",
+    ".github/",
+    ".gradle/",
+    ".idea/",
+    ".settings/",
+    ".vscode/",
+    "__pycache__/",
+    "build/",
+    "env/",
+    "gradle/",
+    "node_modules/",
+    "%.pdb",
+    "%.dll",
+    "%.class",
+    "%.exe",
+    "%.cache",
+    "%.ico",
+    "%.pdf",
+    "%.dylib",
+    "%.jar",
+    "%.docx",
+    "%.met",
+    "smalljre_*/*",
+    ".vale/",
+    "%.burp",
+    "%.mp4",
+    "%.mkv",
+    "%.rar",
+    "%.zip",
+    "%.7z",
+    "%.tar",
+    "%.bz2",
+    "%.epub",
+    "%.flac",
+    "%.tar.gz",
+  },
 }
 
 require("telescope").load_extension "ui-select"
-require("telescope").load_extension "notify"
+-- require("telescope").load_extension "notify"
 require("telescope").load_extension "fzf"
 -- telescope.load_extension "file_browser"
 -- require("telescope.builtin").lsp_code_actions(require("telescope.themes").get_cursor())
