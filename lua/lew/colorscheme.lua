d9e2505d2c0b6b52ca88d3250bd15cbc2829d9f3
lewpoly <ldixon.design@gmail.com>
2022-09-24 10:04:58 -0400

    things

--- lua/lew/colorscheme.lua
+++ lua/lew/colorscheme.lua
@@ -1,4 +1,4 @@
-vim.g.lualew_colorscheme = "gruvbox"
+vim.g.lualew_colorscheme = "sherbet"
 
 function ColorMyPencils()
   vim.g.gruvbox_contrast_dark = "hard"
@@ -16,8 +16,6 @@ function ColorMyPencils()
 -- vim.g.sherbet_italic_conditionals = true
   vim.g.sherbet_italic_comments = true
 
-  -- vim.g.tokyonight_transparent_sidebar = true
-  -- vim.g.tokyonight_transparent = true
   vim.g.nord_contrast = true
   vim.g.nord_borders = true
 
@@ -36,24 +34,28 @@ function ColorMyPencils()
     bg = "none",
   })
 
+  -- hl("Pmenu", {
+  --   bg = "#504945",
+  -- })
+
   hl("ColorColumn", {
     -- ctermbg = 0,
     bg = "#555555",
   })
 
-  -- hl("CursorLineNR", {
-  --   fg = "#5eacd3",
-  --   bg = "None",
-  -- })
+  hl("CursorLineNR", {
+    fg = "#ffffff",
+    bg = "None",
+  })
 
   hl("LineNr", {
     fg = "#5eacd3",
   })
 
-  hl("Normal", {
-    bg = "none",
-    fg = "#ebdbb2"
-  })
+  -- hl("Normal", {
+  --   bg = "none",
+  --   fg = "#ebdbb2"
+  -- })
 
 
   -- hl("Function", {
@@ -97,40 +99,40 @@ end
 --   color_overrides = {},
 --   highlight_overrides = {},
 -- }
--- require("tokyonight").setup {
---   -- your configuration comes here
---   -- or leave it empty to use the default settings
---   style = "storm", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
---   transparent = true, -- Enable this to disable setting the background color
---   terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
---   styles = {
---     -- Style to be applied to different syntax groups
---     -- Value is any valid attr-list value for `:help nvim_set_hl`
---     comments = { italic = true },
---     keywords = { italic = true },
---     functions = {},
---     variables = {},
---     -- Background styles. Can be "dark", "transparent" or "normal"
---     sidebars = "transparent", -- style for sidebars, see below
---     floats = "transparent", -- style for floating windows
---   },
---   sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
---   day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
---   hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
---   dim_inactive = false, -- dims inactive windows
---   lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+require("tokyonight").setup {
+  -- your configuration comes here
+  -- or leave it empty to use the default settings
+  style = "storm", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
+  transparent = true, -- Enable this to disable setting the background color
+  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+  styles = {
+    -- Style to be applied to different syntax groups
+    -- Value is any valid attr-list value for `:help nvim_set_hl`
+    comments = { italic = true },
+    keywords = { italic = true },
+    functions = {},
+    variables = {},
+    -- Background styles. Can be "dark", "transparent" or "normal"
+    sidebars = "transparent", -- style for sidebars, see below
+    floats = "transparent", -- style for floating windows
+  },
+  sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+  hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+  dim_inactive = false, -- dims inactive windows
+  lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
 
---   --- You can override specific color groups to use other groups or a hex color
---   --- function will be called with a ColorScheme table
---   ---@param colors ColorScheme
---   on_colors = function(colors) end,
+  --- You can override specific color groups to use other groups or a hex color
+  --- function will be called with a ColorScheme table
+  ---@param colors ColorScheme
+  on_colors = function(colors) end,
 
---   --- You can override specific highlights to use other groups or a hex color
---   --- function will be called with a Highlights and ColorScheme table
---   ---@param highlights Highlights
---   ---@param colors ColorScheme
---   on_highlights = function(highlights, colors) end,
--- }
+  --- You can override specific highlights to use other groups or a hex color
+  --- function will be called with a Highlights and ColorScheme table
+  ---@param highlights Highlights
+  ---@param colors ColorScheme
+  on_highlights = function(highlights, colors) end,
+}
 ColorMyPencils()
 
 -- local colorscheme = "sherbet"
@@ -158,7 +160,7 @@ ColorMyPencils()
 -- vim.cmd "hi TelescopeNormal guibg=none ctermbg=none"
 -- vim.cmd "hi MsgArea guibg=none ctermbg=none"
 -- vim.cmd "hi SignColumn guibg=none ctermbg=none"
--- vim.cmd "hi EndOfBuffer guibg=none ctermbg=none"
+-- vim.cmd "hi EndOfBuffer guifg=none ctermbg=none"
 -- vim.cmd "hi Normal guibg=NONE ctermbg=NONE"
 -- -- vim.cmd "hi NormalFloat guibg=NONE ctermbg=NONE"
 -- vim.cmd "hi LineNr guibg=NONE ctermbg=NONE"
