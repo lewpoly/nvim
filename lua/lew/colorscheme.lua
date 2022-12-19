c8e3e6b5726aedbfbc084476b7dc8328168742c1
Lew Dixon <ldixon.design@gmail.com>
2022-12-19 12:30:29 -0500

    removed tokyo setup

--- lua/lew/colorscheme.lua
+++ lua/lew/colorscheme.lua
@@ -214,48 +214,48 @@ require("catppuccin").setup {
     -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
   },
 }
-require("tokyonight").setup {
-  -- your configuration comes here
-  -- or leave it empty to use the default settings
-  style = "night", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
-  transparent = false, -- Enable this to disable setting the background color
-  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-  styles = {
-    -- Style to be applied to different syntax groups
-    -- Value is any valid attr-list value for `:help nvim_set_hl`
-    comments = { italic = true },
-    keywords = { italic = true },
-    functions = {},
-    variables = {},
-    -- Background styles. Can be "dark", "transparent" or "normal"
-    sidebars = "transparent", -- style for sidebars, see below
-    floats = "transparent", -- style for floating windows
-  },
-  sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-  hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-  dim_inactive = false, -- dims inactive windows
-  lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
-
-  --- You can override specific color groups to use other groups or a hex color
-  --- function will be called with a ColorScheme table
-  ---@param colors ColorScheme
-  on_colors = function(colors)
-    -- colors.bg = "#181818"
-    -- colors.bg_sidebar = "#202329"
-    -- colors.comment = "#808080"
-    -- colors.green = "#d0f7a6"
-    -- colors.green1 = "#8fbcbb"
-    -- colors.fg = "#d4d4d4"
-    -- colors.border_highlight = "#d4d4d4"
-  end,
-
-  --- You can override specific highlights to use other groups or a hex color
-  --- function will be called with a Highlights and ColorScheme table
-  ---@param highlights Highlights
-  ---@param colors ColorScheme
-  on_highlights = function(highlights, colors) end,
-}
+-- require("tokyonight").setup {
+--   -- your configuration comes here
+--   -- or leave it empty to use the default settings
+--   style = "night", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
+--   transparent = false, -- Enable this to disable setting the background color
+--   terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+--   styles = {
+--     -- Style to be applied to different syntax groups
+--     -- Value is any valid attr-list value for `:help nvim_set_hl`
+--     comments = { italic = true },
+--     keywords = { italic = true },
+--     functions = {},
+--     variables = {},
+--     -- Background styles. Can be "dark", "transparent" or "normal"
+--     sidebars = "transparent", -- style for sidebars, see below
+--     floats = "transparent", -- style for floating windows
+--   },
+--   sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+--   day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+--   hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+--   dim_inactive = false, -- dims inactive windows
+--   lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+--
+--   --- You can override specific color groups to use other groups or a hex color
+--   --- function will be called with a ColorScheme table
+--   ---@param colors ColorScheme
+--   on_colors = function(colors)
+--     -- colors.bg = "#181818"
+--     -- colors.bg_sidebar = "#202329"
+--     -- colors.comment = "#808080"
+--     -- colors.green = "#d0f7a6"
+--     -- colors.green1 = "#8fbcbb"
+--     -- colors.fg = "#d4d4d4"
+--     -- colors.border_highlight = "#d4d4d4"
+--   end,
+--
+--   --- You can override specific highlights to use other groups or a hex color
+--   --- function will be called with a Highlights and ColorScheme table
+--   ---@param highlights Highlights
+--   ---@param colors ColorScheme
+--   on_highlights = function(highlights, colors) end,
+-- }
 ColorMyPencils()
 
 -- local colorscheme = "sherbet"
