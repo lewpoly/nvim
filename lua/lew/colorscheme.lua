fe6aef2242fc17f033240b4d94ab6f88cc95f693
lewpoly <ldixon.design@gmail.com>
2023-05-26 11:17:52 -0400

    material

--- lua/lew/colorscheme.lua
+++ lua/lew/colorscheme.lua
@@ -7,6 +7,6 @@ vim.g.gruvbox_material_foreground = "material" -- material, mix, original
 -- everforest
 vim.g.everforest_background = "hard" -- hard, medium, soft
 
-vim.cmd.colorscheme "everforest"
+vim.cmd.colorscheme "gruvbox-material"
 
 -- vim.api.nvim_set_hl(0, "Normal", { bg = "#1d2021" })
